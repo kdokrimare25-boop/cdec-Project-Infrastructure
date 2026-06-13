20d5f9d8744e884caa20eef76e440cbe9528b910
Kamini Dokrimare <kamini@gmail.com>
2026-06-13 16:27:26 +0530

    updated

--- infrastructure/frontend/backend.tf
+++ infrastructure/frontend/backend.tf
@@ -6,7 +6,7 @@
 
 terraform {
   backend "s3" {
-    bucket  = "cdec-alpha-terraform-state-kaminid/frontend/"
+    bucket  = "cdec-alpha-terraform-state-kaminid"
     key     = "frontend/terraform.tfstate"
     region  = "eu-north-1"
     encrypt = true
